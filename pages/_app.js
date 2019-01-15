48526c5eb88fefffecd8b77650183411ddb8e029
raulvictorrosa <raulvictorrosa@gmail.com>
2019-01-15 12:55:27 -0200

    feat: Add _app.js

---
+++ pages/_app.js
@@ -0,0 +1,35 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import App, { Container } from "next/app";
+import React from "react";
+import auth0 from "../services/auth0";
+import "../styles/main.scss";
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+    const user = process.browser
+      ? auth0.clientAuth()
+      : auth0.serverAuth(ctx.req);
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    const auth = {
+      user,
+      isAuthenticated: !!user
+    };
+
+    return { pageProps, auth };
+  }
+
+  render() {
+    const { Component, pageProps, auth } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} auth={auth} />
+      </Container>
+    );
+  }
+}
