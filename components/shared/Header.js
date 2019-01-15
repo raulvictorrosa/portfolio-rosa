503fbcd93849239522dc6a91c0dd060380207230
raulvictorrosa <raulvictorrosa@gmail.com>
2019-01-15 12:55:12 -0200

    feat: Update header with reactstrap

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,42 +1,104 @@
 import Link from "next/link";
-import { Component } from "react";
-import "../../styles/main.scss";
+import React from "react";
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem
+} from "reactstrap";
+import auth0 from "../../services/auth0";
 
-class Header extends Component {
+const BsNavLink = props => {
+  const { route, title } = props;
+  return (
+    <Link href={route}>
+      <a className="nav-link port-navbar-link">{title}</a>
+    </Link>
+  );
+};
+
+const Login = () => {
+  return (
+    <span onClick={auth0.login} className="nav-link port-navbar-link clickable">
+      Login
+    </span>
+  );
+};
+
+const Logout = () => {
+  return (
+    <span
+      onClick={auth0.logout}
+      className="nav-link port-navbar-link clickable"
+    >
+      Logout
+    </span>
+  );
+};
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    // debugger;
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
   render() {
-    const { title } = this.props;
+    const { isAuthenticated } = this.props;
     return (
-      <>
-        <p>{title}</p>
-        {this.props.children}
-        <p className="customClass">I am styled P elemtent</p>
-        <p className="customClassFromFile">I am styled P elemtent</p>
-        <Link href="/">
-          <a style={{ fontSize: "20px" }}>Home</a>
-        </Link>
-        <Link href="/about">
-          <a>About</a>
-        </Link>
-        <Link href="/portifolios">
-          <a>Portifolio</a>
-        </Link>
-        <Link href="/blogs">
-          <a>Blogs</a>
-        </Link>
-        <Link href="/cv">
-          <a>CV</a>
-        </Link>
-        <style jsx="true">{`
-          a {
-            font-size: 20px;
-          }
-          .customClass {
-            color: red;
-          }
-        `}</style>
-      </>
+      <div>
+        <Navbar
+          className="port-navbar port-default position-absolute"
+          color="transparent"
+          dark
+          expand="md"
+        >
+          <NavbarBrand className="port-navbar-brand" href="/">
+            Raul Rosa
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem className="port-navbar-item">
+                <BsNavLink route="/" title="Home" />
+              </NavItem>
+              <NavItem className="port-navbar-item">
+                <BsNavLink route="/about" title="About" />
+              </NavItem>
+              <NavItem className="port-navbar-item">
+                <BsNavLink route="/portifolios" title="Portifolio" />
+              </NavItem>
+              <NavItem className="port-navbar-item">
+                <BsNavLink route="/blogs" title="Blogs" />
+              </NavItem>
+              <NavItem className="port-navbar-item">
+                <BsNavLink route="/cv" title="CV" />
+              </NavItem>
+              {!isAuthenticated ? (
+                <NavItem className="port-navbar-item">
+                  <Login />
+                </NavItem>
+              ) : (
+                <NavItem className="port-navbar-item">
+                  <Logout />
+                </NavItem>
+              )}
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
-
-export default Header;
