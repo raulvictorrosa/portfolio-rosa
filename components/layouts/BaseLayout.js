0906dc566e0ff876b85c05baf0632a1c35600051
raulvictorrosa <raulvictorrosa@gmail.com>
2019-01-15 12:53:49 -0200

    feat: Update Base Layout passing authentication as props in Header component

--- components/layouts/BaseLayout.js
+++ components/layouts/BaseLayout.js
@@ -1,11 +1,14 @@
 import Header from "../shared/Header";
 
 const BaseLayout = props => {
+  const { className, children, isAuthenticated } = props;
   return (
-    <>
-      <Header />
-      {props.children}
-    </>
+    <div className="layout-container">
+      <Header isAuthenticated={isAuthenticated} />
+      <main className={`cover ${className || ""}`}>
+        <div className="wrapper">{children}</div>
+      </main>
+    </div>
   );
 };
 
