a02eaec34057a8314892660f304eb71573eef4d6
raulvictorrosa <raulvictorrosa@gmail.com>
2019-01-15 12:57:27 -0200

    feat: Callback page to wait for user authentication

---
+++ pages/callback.js
@@ -0,0 +1,24 @@
+import { withRouter } from "next/router";
+import { Component } from "react";
+import BasePage from "../components/BasePage";
+import BaseLayout from "../components/layouts/BaseLayout";
+import auth0Client from "../services/auth0";
+
+class Callback extends Component {
+  async componentDidMount() {
+    await auth0Client.handleAuthentication();
+    this.props.router.push("/");
+  }
+
+  render() {
+    return (
+      <BaseLayout>
+        <BasePage>
+          <h1>Verifying logging data...</h1>
+        </BasePage>
+      </BaseLayout>
+    );
+  }
+}
+
+export default withRouter(Callback);
