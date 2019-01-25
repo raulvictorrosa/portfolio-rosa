0e64d1679bd0fa2705a070b32b9a51f4498f7e3a
raulvictorrosa <raulvictorrosa@gmail.com>
2019-01-25 11:37:11 -0200

    feat: Check for token in authorization handler

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,13 +1,16 @@
-// MIDDLEWARE
-exports.checkJWT = function(req, res, next) {
-  const isValidToken = true;
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
 
-  if (isValidToken) {
-    next();
-  } else {
-    return res.status(401).send({
-      title: "Not Authorized",
-      detail: "Please login in order to get a data"
-    });
-  }
-};
+// MIDDLEWARE
+exports.checkJWT = jwt({
+  secret: jwksRsa({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 15,
+    jwksUri: "https://raulvictorrosa.auth0.com/.well-known/jwks.json"
+  }),
+
+  audience: "mnTg7fHPDymU2fMuujkaBavVg1lOmg7k",
+  issuer: "http://raulvictorrosa.auth0.com",
+  algorithms: ["RS256"]
+});
