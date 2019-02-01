10b70551e799ff37bcdcde48941200712f45c015
raulvictorrosa <raulvictorrosa@gmail.com>
2019-02-01 09:15:19 -0200

    feat: Portifolio card styling

--- pages/portifolios.js
+++ pages/portifolios.js
@@ -1,8 +1,16 @@
 import axios from "axios";
-import { Component } from "react";
+import { Component, Fragment } from "react";
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardText,
+  CardTitle,
+  Col,
+  Row
+} from "reactstrap";
 import BasePage from "../components/BasePage";
 import BaseLayout from "../components/layouts/BaseLayout";
-import { Link } from "../routes";
 
 class Portifolio extends Component {
   static async getInitialProps() {
@@ -21,11 +29,27 @@ class Portifolio extends Component {
 
   renderPosts(posts) {
     return posts.map((post, index) => (
-      <li key={index}>
-        <Link route={`/portifolio/${post.id}`}>
-          <a>{post.title}</a>
-        </Link>
-      </li>
+      <Col md="4">
+        <Fragment key={index}>
+          <span>
+            <Card className="portfolio-card">
+              <CardHeader className="portfolio-card-header">
+                Some Position {index}
+              </CardHeader>
+              <CardBody>
+                <p className="portfolio-card-city"> Some Location {index} </p>
+                <CardTitle className="portfolio-card-title">
+                  Some Company {index}
+                </CardTitle>
+                <CardText className="portfolio-card-text">
+                  Some Description {index}
+                </CardText>
+                <div className="readMore"> </div>
+              </CardBody>
+            </Card>
+          </span>
+        </Fragment>
+      </Col>
     ));
   }
 
@@ -34,8 +58,8 @@ class Portifolio extends Component {
     // console.log(this.props);
     return (
       <BaseLayout {...this.props.auth}>
-        <BasePage title="Portifolio">
-          <ul>{this.renderPosts(posts)}</ul>
+        <BasePage className="portfolio-page" title="Portifolio">
+          <Row>{this.renderPosts(posts)}</Row>
         </BasePage>
       </BaseLayout>
     );
