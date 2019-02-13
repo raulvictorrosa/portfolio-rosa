59a2cad0146282f27f91248ff749eaefd2192a96
raulvictorrosa <raulvictorrosa@gmail.com>
2019-02-13 10:06:49 -0200

    feat: Simple Formik form

--- components/portifolios/PortifolioCreateForm.js
+++ components/portifolios/PortifolioCreateForm.js
@@ -1,69 +1,41 @@
-import React, { Component } from "react";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import React from "react";
 
-export default class PortifolioCreateForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: "",
-      description: "",
-      language: ""
-    };
+const PortifolioCreateForm = () => (
+  <div>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={values => {
+        let errors = {};
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
+          <Field type="email" name="email" />
+          <ErrorMessage name="email" component="div" />
+          <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  handleSubmit(event) {
-    alert(
-      "A name was submitted: " +
-        this.state.title +
-        " " +
-        this.state.description +
-        " " +
-        this.state.language
-    );
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            name="title"
-            type="text"
-            value={this.state.title}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Essay:
-          <textarea
-            name="description"
-            value={this.state.description}
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          Pick your favorite language:
-          <select
-            name="language"
-            value={this.state.language}
-            onChange={this.handleChange}
-          >
-            <option value="javascript">JavaScript</option>
-            <option value="java">Java</option>
-            <option value="c++">C++</option>
-            <option value="c#">C#</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
+export default PortifolioCreateForm;
