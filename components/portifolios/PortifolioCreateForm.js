d3bd5629cd5fc0a62f1966b9bcd36bc873d8905d
raulvictorrosa <raulvictorrosa@gmail.com>
2019-02-13 09:05:42 -0200

    feat: More input types

--- components/portifolios/PortifolioCreateForm.js
+++ components/portifolios/PortifolioCreateForm.js
@@ -3,18 +3,39 @@ import React, { Component } from "react";
 export default class PortifolioCreateForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { title: "" };
+    this.state = {
+      title: "",
+      description: "",
+      language: ""
+    };
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleChangeDescription = this.handleChangeDescription.bind(this);
+    this.handleChangeLanguage = this.handleChangeLanguage.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ title: event.target.title });
+    this.setState({ title: event.target.value });
+  }
+
+  handleChangeDescription(event) {
+    this.setState({ description: event.target.value });
+  }
+
+  handleChangeLanguage(event) {
+    this.setState({ language: event.target.value });
   }
 
   handleSubmit(event) {
-    alert("A name was submitted: " + this.state.title);
+    alert(
+      "A name was submitted: " +
+        this.state.title +
+        " " +
+        this.state.description +
+        " " +
+        this.state.language
+    );
     event.preventDefault();
   }
 
@@ -28,6 +49,25 @@ export default class PortifolioCreateForm extends Component {
             value={this.state.title}
             onChange={this.handleChange}
           />
+        </label>
+        <label>
+          Essay:
+          <textarea
+            value={this.state.description}
+            onChange={this.handleChangeDescription}
+          />
+        </label>
+        <label>
+          Pick your favorite language:
+          <select
+            value={this.state.language}
+            onChange={this.handleChangeLanguage}
+          >
+            <option value="javascript">JavaScript</option>
+            <option value="java">Java</option>
+            <option value="c++">C++</option>
+            <option value="c#">C#</option>
+          </select>
         </label>
         <input type="submit" value="Submit" />
       </form>
