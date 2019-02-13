0e3d553f5eb7f535bbdeab6d56fe0159e10dbadf
raulvictorrosa <raulvictorrosa@gmail.com>
2019-02-13 13:14:14 -0200

    feat: Some specific inputs with Formik

--- components/portifolios/PortifolioCreateForm.js
+++ components/portifolios/PortifolioCreateForm.js
@@ -1,21 +1,31 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
 
+const validateInputs = validate => {
+  let errors = {};
+  // if (!validate.email) {
+  //   errors.email = "Required";
+  // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(validate.email)) {
+  //   errors.email = "Invalid email address";
+  // }
+  return errors;
+};
+
+const INITIAL_VALUES = {
+  title: "",
+  company: "",
+  location: "",
+  position: "",
+  description: "",
+  startDate: "",
+  endDate: ""
+};
+
 const PortifolioCreateForm = () => (
   <div>
     <Formik
-      initialValues={{ email: "", password: "" }}
-      validate={values => {
-        let errors = {};
-        if (!values.email) {
-          errors.email = "Required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address";
-        }
-        return errors;
-      }}
+      initialValues={INITIAL_VALUES}
+      validate={values => validateInputs(values)}
       onSubmit={(values, { setSubmitting }) => {
         setTimeout(() => {
           alert(JSON.stringify(values, null, 2));
@@ -25,13 +35,46 @@ const PortifolioCreateForm = () => (
     >
       {({ isSubmitting }) => (
         <Form>
-          <Field type="email" name="email" />
-          <ErrorMessage name="email" component="div" />
-          <Field type="password" name="password" />
-          <ErrorMessage name="password" component="div" />
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
+          <div>
+            <label>Title</label>
+            <Field type="text" name="title" />
+            <ErrorMessage name="title" component="div" />
+          </div>
+          <div>
+            <label>Company</label>
+            <Field type="text" name="company" />
+            <ErrorMessage name="company" component="div" />
+          </div>
+          <div>
+            <label>Location</label>
+            <Field type="text" name="location" />
+            <ErrorMessage name="location" component="div" />
+          </div>
+          <div>
+            <label>Position</label>
+            <Field type="text" name="position" />
+            <ErrorMessage name="position" component="div" />
+          </div>
+          <div>
+            <label>Description</label>
+            <Field type="textarea" name="description" component="textarea" />
+            <ErrorMessage name="description" component="div" />
+          </div>
+          <div>
+            <label>Start Date</label>
+            <Field type="text" name="startDate" />
+            <ErrorMessage name="startDate" component="div" />
+          </div>
+          <div>
+            <label>End Date</label>
+            <Field type="text" name="endDate" />
+            <ErrorMessage name="endDate" component="div" />
+          </div>
+          <div>
+            <button type="submit" disabled={isSubmitting}>
+              Create
+            </button>
+          </div>
         </Form>
       )}
     </Formik>
