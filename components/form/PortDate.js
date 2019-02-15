b7ac196427046cfdbe8877a06c2fac35baff7dc5
raulvictorrosa <raulvictorrosa@gmail.com>
2019-02-15 13:08:05 -0200

    feat: On change event on Formik

--- components/form/PortDate.js
+++ components/form/PortDate.js
@@ -14,11 +14,15 @@ export default class PortDate extends React.Component {
   }
 
   handleChange(date) {
-    debugger;
-    const formattedDate = date.format();
+    const { setFieldValue, setFieldTouched } = this.props.form;
+    const { name } = this.props.field;
+
     this.setState({
       dateValue: date
     });
+
+    setFieldValue(name, date, true);
+    setFieldTouched(name, true, true);
   }
 
   render() {
--- components/form/PortInput.js
+++ components/form/PortInput.js
@@ -6,14 +6,16 @@ const PortInput = ({
   field, // { name, value, onChange, onBlur }
   form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
   ...props
-}) => (
-  <FormGroup>
-    <Label>{label}</Label>
-    <Input type={type} {...field} {...props} />
-    {touched[field.name] && errors[field.name] && (
-      <div className="error">{errors[field.name]}</div>
-    )}
-  </FormGroup>
-);
+}) => {
+  return (
+    <FormGroup>
+      <Label>{label}</Label>
+      <Input type={type} {...field} {...props} />
+      {touched[field.name] && errors[field.name] && (
+        <div className="error">{errors[field.name]}</div>
+      )}
+    </FormGroup>
+  );
+};
 
 export default PortInput;
