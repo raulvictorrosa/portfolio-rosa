a09450f5092f94bf1dc881fe16b2ed58bac0a469
Raul Rosa <raulvictorrosa@gmail.com>
2020-10-25 22:09:22 -0300

    feat: server.js

--- server/index.js
+++ server/index.js
@@ -1,53 +1,34 @@
-const express = require("express");
-const next = require("next");
-const routes = require("../routes");
+const express = require('express')
+const next = require('next')
 
-// SERVICES
-const authServices = require("./services/auth");
-
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = routes.getRequestHandler(app);
-
-const secretData = [
-  { title: "SecretData 1", description: "Plans how to build spaceship" },
-  { title: "SecretData 2", description: "Plans how to build spaceship" }
-];
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 app
   .prepare()
   .then(() => {
-    const server = express();
+    const server = express()
 
-    server.get("/api/v1/secret", authServices.checkJWT, (req, res) => {
-      return res.json(secretData);
-    });
+    server.all('*', (req, res) => {
+      return handle(req, res)
+    })
 
-    server.get(
-      "/api/v1/onlysiteowner",
-      authServices.checkJWT,
-      authServices.checkRole("siteOwner"),
-      (req, res) => res.json(secretData)
-    );
+    // server.use(function (err, req, res, next) {
+    //   if (err.name === 'UnauthorizedError') {
+    //     res
+    //       .status(401)
+    //       .send({ title: 'Unauthorized', detail: 'Unauthorized Access' });
+    //   }
+    // });
 
-    server.get("*", (req, res) => {
-      return handle(req, res);
-    });
-
-    server.use(function(err, req, res, next) {
-      if (err.name === "UnauthorizedError") {
-        res
-          .status(401)
-          .send({ title: "Unauthorized", detail: "Unauthorized Access" });
-      }
-    });
-
-    server.use(handle).listen(3000, err => {
-      if (err) throw err;
-      console.log("> Ready on http://localhost:3000");
-    });
+    const PORT = process.env.PORT || 3000
+    server.use(handle).listen(PORT, (err) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${PORT}`)
+    })
   })
-  .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
+  .catch((ex) => {
+    console.error(ex.stack)
+    process.exit(1)
+  })
