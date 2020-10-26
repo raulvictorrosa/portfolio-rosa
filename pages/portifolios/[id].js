b3d9c45c685b21b2441c46a086273bc683f4f478
Raul Rosa <raulvictorrosa@gmail.com>
2020-10-25 22:36:17 -0300

    feat: single portifolio page

--- pages/portifolios/[id].js
+++ pages/portifolios/[id].js
@@ -1,28 +1,29 @@
+import { useGetData } from '@/actions'
 import BasePage from '@/components/BasePage'
 import BaseLayout from '@/components/layouts/BaseLayout'
-import axios from 'axios'
+import { useRouter } from 'next/router'
 
-const Portifolio = ({ portifolio }) => (
-  <BaseLayout>
-    <BasePage>
-      <h1>{portifolio.title}</h1>
-      <p>{portifolio.body}</p>
-      <p>{portifolio.id}</p>
-    </BasePage>
-  </BaseLayout>
-)
+const Portifolio = () => {
+  const router = useRouter()
+  const { data: portifolio, error, loading } = useGetData(
+    router.query.id ? `/api/v1/posts/${router.query.id}` : null
+  )
 
-Portifolio.getInitialProps = async ({ query }) => {
-  let portifolio = {}
-  try {
-    const res = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts/${query.id}`
-    )
-    portifolio = res.data
-  } catch (error) {
-    console.error(error)
-  }
-  return { portifolio }
+  return (
+    <BaseLayout>
+      <BasePage>
+        {loading && <p>Loading Data...</p>}
+        {error && <div className="alert alert-danger">{error.message}</div>}
+        {portifolio && (
+          <>
+            <h1>{portifolio.title}</h1>
+            <p>{portifolio.body}</p>
+            <p>{portifolio.id}</p>
+          </>
+        )}
+      </BasePage>
+    </BaseLayout>
+  )
 }
 
 export default Portifolio
