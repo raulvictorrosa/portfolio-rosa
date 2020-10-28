becc910ffbfd2fac08f051f83b99d655cd67d522
Raul Rosa <raulvictorrosa@gmail.com>
2020-10-28 13:33:32 -0300

    chore: next config to read dotenv files

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname)
+    config.plugins.push(new Dotenv({ silent: true }))
+
     return config
   }
 }
