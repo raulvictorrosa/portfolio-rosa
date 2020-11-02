2b7bdce64c389e12d8e5f0d360425f0031b981c6
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-02 15:24:29 -0300

    feat: handle callback from authentication api

--- pages/api/v1/callback.js
+++ pages/api/v1/callback.js
@@ -3,7 +3,6 @@ import auth0 from '@/utils/auth0'
 export default async function callback(req, res) {
   try {
     await auth0.handleCallback(req, res, { redirectTo: '/' })
-    // res.status(200).json(auth0)
   } catch (error) {
     console.error(error)
     res.status(error.status || 400).end(error.message)
