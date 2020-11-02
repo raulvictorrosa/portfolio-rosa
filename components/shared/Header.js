f4dc934cb2393d3c819cfb85e4a206456ceedae3
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-02 15:30:15 -0300

    feat: Header component

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,104 +1,96 @@
-import Link from "next/link";
-import React from "react";
-import {
-  Collapse,
-  Nav,
-  Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  NavItem
-} from "reactstrap";
-import auth0 from "../../services/auth0";
+import Link from 'next/link'
+import { useState } from 'react'
+import { Collapse, Nav, Navbar, NavbarToggler, NavItem } from 'reactstrap'
 
-const BsNavLink = props => {
-  const { route, title } = props;
+const BsNavLink = (props) => {
+  const { href, title } = props
   return (
-    <Link href={route}>
+    <Link href={href}>
       <a className="nav-link port-navbar-link">{title}</a>
     </Link>
-  );
-};
-
-const Login = () => {
-  return (
-    <span onClick={auth0.login} className="nav-link port-navbar-link clickable">
-      Login
-    </span>
-  );
-};
-
-const Logout = () => {
-  return (
-    <span
-      onClick={auth0.logout}
-      className="nav-link port-navbar-link clickable"
-    >
-      Logout
-    </span>
-  );
-};
-
-export default class Header extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-
-  toggle() {
-    // debugger;
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-
-  render() {
-    const { isAuthenticated, user, className } = this.props;
-    return (
-      <div>
-        <Navbar
-          className={`port-navbar port-nav-base position-absolute ${className}`}
-          color="transparent"
-          dark
-          expand="md"
-        >
-          <NavbarBrand className="port-navbar-brand" href="/">
-            Raul Rosa
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem className="port-navbar-item">
-                <BsNavLink route="/" title="Home" />
-              </NavItem>
-              <NavItem className="port-navbar-item">
-                <BsNavLink route="/about" title="About" />
-              </NavItem>
-              <NavItem className="port-navbar-item">
-                <BsNavLink route="/portifolios" title="Portifolio" />
-              </NavItem>
-              <NavItem className="port-navbar-item">
-                <BsNavLink route="/blogs" title="Blogs" />
-              </NavItem>
-              <NavItem className="port-navbar-item">
-                <BsNavLink route="/cv" title="CV" />
-              </NavItem>
-              {!isAuthenticated ? (
-                <NavItem className="port-navbar-item">
-                  <Login />
-                </NavItem>
-              ) : (
-                <NavItem className="port-navbar-item">
-                  <Logout />
-                </NavItem>
-              )}
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
-    );
-  }
+  )
 }
+
+const BsNavBrand = (props) => (
+  <Link href="/">
+    <a className="navbar-brand port-navbar-brand">Raul Rosa</a>
+  </Link>
+)
+
+const LoginLink = () => (
+  // <span className="nav-link port-navbar-link clickable">Login</span>
+  <a className="nav-link port-navbar-link" href="/api/v1/login">
+    Login
+  </a>
+  // <BsNavLink href="/api/v1/login" title="Login" />
+)
+
+const LogoutLink = () => (
+  // <span className="nav-link port-navbar-link clickable">Logout</span>
+  <a className="nav-link port-navbar-link" href="/api/v1/logout">
+    Logout
+  </a>
+)
+
+const Header = ({ user, loading }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggle = () => setIsOpen(!isOpen)
+
+  const { className } = ''
+  return (
+    <div>
+      <Navbar
+        className={`port-navbar port-default position-absolute ${className}`}
+        color="transparent"
+        dark
+        expand="md"
+      >
+        <BsNavBrand />
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/" title="Home" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/about" title="About" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/portifolios" title="Portifolio" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/blogs" title="Blogs" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/cv" title="CV" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/secret" title="Secret" />
+            </NavItem>
+            <NavItem className="port-navbar-item">
+              <BsNavLink href="/secretssr" title="SecretSSR" />
+            </NavItem>
+          </Nav>
+          <Nav navbar>
+            {!loading && (
+              <>
+                {!user && (
+                  <NavItem className="port-navbar-item">
+                    <LoginLink />
+                  </NavItem>
+                )}
+                {user && (
+                  <NavItem className="port-navbar-item">
+                    <LogoutLink />
+                  </NavItem>
+                )}
+              </>
+            )}
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  )
+}
+
+export default Header
