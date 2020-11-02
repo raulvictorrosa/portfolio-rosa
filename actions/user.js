8c4c1ce8d7fd7af675fe542e2149e61baa6c2b78
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-02 15:23:08 -0300

    feat: get user information from api

---
+++ actions/user.js
@@ -0,0 +1,8 @@
+import { fetcher } from '@/actions'
+import useSWR from 'swr'
+
+export const useGetUser = (user) => {
+  const { data, error, ...rest } = useSWR('/api/v1/me', fetcher)
+
+  return { data, error, loading: !data && !error, ...rest }
+}
