a831a58e43f2e12ced1e594067e8407fcf92a574
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-02 15:27:08 -0300

    feat: handle authotized user from Auth0

--- utils/auth0.js
+++ utils/auth0.js
@@ -1,6 +1,6 @@
 import { initAuth0 } from '@auth0/nextjs-auth0'
 
-export default initAuth0({
+const auth0 = initAuth0({
   domain: process.env.AUTH0_DOMAIN,
   clientId: process.env.AUTH0_CLIENT_ID,
   clientSecret: process.env.AUTH0_CLIENT_SECRET,
@@ -11,3 +11,16 @@ export default initAuth0({
     cookieSecret: process.env.AUTH0_COOKIE_SECRET
   }
 })
+
+export default auth0
+
+export const authorizedUser = async (req, res) => {
+  const session = await auth0.getSession(req)
+  if (!session || !session.user) {
+    res.writeHead(302, { Location: '/api/v1/login' })
+    res.end()
+    return null
+  }
+
+  return session.user
+}
