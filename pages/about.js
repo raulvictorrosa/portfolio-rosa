5db0012b1c0d30a991b6bddf2450a2eb2fdae48c
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-02 15:20:25 -0300

    feat: About page

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,14 @@
+import { useGetUser } from '@/actions/user'
 import BasePage from '@/components/BasePage'
-// import withAuth from '@/components/hoc/withAuth'
 import BaseLayout from '@/components/layouts/BaseLayout'
 
-const About = () => (
-  <BaseLayout>
-    <BasePage className="about-page" title="I am About Page" />
-  </BaseLayout>
-)
+const About = () => {
+  const { data, loading } = useGetUser()
+  return (
+    <BaseLayout user={data} loading={loading}>
+      <BasePage className="about-page" title="I am About Page" />
+    </BaseLayout>
+  )
+}
 
-// export default withAuth()(About)
 export default About
