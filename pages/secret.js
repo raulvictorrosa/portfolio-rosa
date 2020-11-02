462f53c0a6425a4e101ca5a98c51e3e8ddd284ac
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-02 15:16:05 -0300

    feat: Basic Secret page

--- pages/secret.js
+++ pages/secret.js
@@ -1,63 +1,16 @@
-import { Component } from "react";
-import { getSecretData } from "../actions";
-import BasePage from "../components/BasePage";
-import withAuth from "../components/hoc/withAuth";
-import BaseLayout from "../components/layouts/BaseLayout";
+import BasePage from '@/components/BasePage'
+import BaseLayout from '@/components/layouts/BaseLayout'
+import withAuth from '@/hoc/withAuth'
 
-class Secret extends Component {
-  static async getInitialProps({ req }) {
-    const anotherSecretData = await getSecretData(req);
-
-    return { anotherSecretData };
-  }
-
-  // constructor() {
-  //   super();
-
-  //   this.state = { secretData: [] };
-  // }
-
-  state = { secretData: [] };
-
-  async componentDidMount() {
-    const secretData = await getSecretData();
-
-    this.setState({
-      secretData
-    });
-  }
-
-  displaySecretData() {
-    const { secretData } = this.state;
-
-    if (secretData && secretData.length > 0) {
-      return secretData.map((data, index) => {
-        return (
-          <div key={index}>
-            <p>{data.title}</p>
-            <p>{data.description}</p>
-          </div>
-        );
-      });
-    }
-
-    return null;
-  }
-
-  render() {
-    const { superSecretValue } = this.props;
-
-    return (
-      <BaseLayout {...this.props.auth}>
-        <BasePage>
-          <h1>I am Secret Page</h1>
-          <p>Secret Content Here</p>
-          <h2>{superSecretValue}</h2>
-          {this.displaySecretData()}
-        </BasePage>
-      </BaseLayout>
-    );
-  }
+const Secret = ({ user, loading }) => {
+  return (
+    <BaseLayout user={user} loading={loading}>
+      <BasePage className="secret-page" title="I am About Page">
+        <h1>I am Secret Page</h1>
+        <p>Secret Content Here - Hello {user.name}</p>
+      </BasePage>
+    </BaseLayout>
+  )
 }
 
-export default withAuth()(Secret);
+export default withAuth(Secret)
