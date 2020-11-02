184e8cd94c4cff5121389b7d6a523601db4cf74d
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-02 15:22:15 -0300

    feat: High-Order Component to handle with Authentication for user

--- hoc/withAuth.js
+++ hoc/withAuth.js
@@ -1,58 +1,78 @@
-import React from "react";
-import BasePage from "../BasePage";
-import BaseLayout from "../layouts/BaseLayout";
+import { useGetUser } from '@/actions/user'
+import Redirect from '@/components/shared/Redirect'
+import React from 'react'
+// import BasePage from "../BasePage";
+// import BaseLayout from "../layouts/BaseLayout";
 
-const namespace = "http://localhost:3000/";
+// const namespace = "http://localhost:3000/";
 
-export default role => Component =>
-  class withAtuh extends React.Component {
-    static async getInitialProps(args) {
-      const pageProps =
-        (await Component.getInitialProps) && Component.getInitialProps(args);
+const withAuth = (Component) => {
+  return (props) => {
+    const { data, loading } = useGetUser()
 
-      return { ...pageProps };
+    if (loading) {
+      return <p>Loading...</p>
     }
 
-    renderProtectedPage() {
-      const { isAuthenticated, user } = this.props.auth;
-      const userRole = user && user[`${namespace}role`];
-      let isAuthorized = false;
-
-      if (role) {
-        if (userRole && userRole === role) {
-          isAuthorized = true;
-        }
-      } else {
-        isAuthorized = true;
-      }
-
-      if (!isAuthenticated) {
-        return (
-          <BaseLayout {...this.props.auth}>
-            <BasePage>
-              <h1>
-                You are not authenticated. Please Login to access this page.
-              </h1>
-            </BasePage>
-          </BaseLayout>
-        );
-      } else if (!isAuthorized) {
-        return (
-          <BaseLayout {...this.props.auth}>
-            <BasePage>
-              <h1>
-                You are not authorized. You don't have permission to visit this
-                page.
-              </h1>
-            </BasePage>
-          </BaseLayout>
-        );
-      } else {
-        return <Component {...this.props} />;
-      }
+    if (!data) {
+      return <Redirect ssr to="/api/v1/login" />
+    } else {
+      return <Component user={data} loading={loading} {...props} />
     }
+  }
+}
 
-    render() {
-      return this.renderProtectedPage();
-    }
-  };
+export default withAuth
+
+// export default role => Component =>
+//   class withAtuh extends React.Component {
+//     static async getInitialProps(args) {
+//       const pageProps =
+//         (await Component.getInitialProps) && Component.getInitialProps(args);
+
+//       return { ...pageProps };
+//     }
+
+//     renderProtectedPage() {
+//       const { isAuthenticated, user } = this.props.auth;
+//       const userRole = user && user[`${namespace}role`];
+//       let isAuthorized = false;
+
+//       if (role) {
+//         if (userRole && userRole === role) {
+//           isAuthorized = true;
+//         }
+//       } else {
+//         isAuthorized = true;
+//       }
+
+//       if (!isAuthenticated) {
+//         return (
+//           <BaseLayout {...this.props.auth}>
+//             <BasePage>
+//               <h1>
+//                 You are not authenticated. Please Login to access this page.
+//               </h1>
+//             </BasePage>
+//           </BaseLayout>
+//         );
+//       } else if (!isAuthorized) {
+//         return (
+//           <BaseLayout {...this.props.auth}>
+//             <BasePage>
+//               <h1>
+//                 You are not authorized. You don't have permission to visit this
+//                 page.
+//               </h1>
+//             </BasePage>
+//           </BaseLayout>
+//         );
+//       } else {
+//         return <Component {...this.props} />;
+//       }
+//     }
+
+//     render() {
+//       return this.renderProtectedPage();
+//     }
+//   };
