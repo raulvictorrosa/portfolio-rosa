266a0e251a997e040e5faa3403ab1771c633aa8a
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-02 15:19:20 -0300

    feat: Simple Secret Page with Server Side Render

--- pages/secretssr.js
+++ pages/secretssr.js
@@ -1,22 +1,37 @@
 import BasePage from '@/components/BasePage'
 import BaseLayout from '@/components/layouts/BaseLayout'
-import { authorizedUser } from '@/utils/auth0'
+import { withAuth } from '@/utils/auth0'
+// import { authorizedUser, withAuth } from '@/utils/auth0'
 
-const SecretSSR = ({ user }) => {
+const SecretSSR = ({ user, title }) => {
   return (
     <BaseLayout user={user} loading={false}>
       <BasePage className="secrete-page" title="I am About Page">
         <h1>I am Secret Page</h1>
         <p>Secret Content Here - Hello {user && user.name}</p>
+        <h2>{title}</h2>
       </BasePage>
     </BaseLayout>
   )
 }
 
-export const getServerSideProps = async ({ req, res }) => {
-  const user = await authorizedUser(req, res)
+// export const getServerSideProps = async ({ req, res }) => {
+//   const user = await authorizedUser(req, res)
 
-  return { props: { user } }
+//   return { props: { user } }
+// }
+
+const getTitle = () => {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res({ title: 'My new title!' })
+    }, 500)
+  })
 }
+
+export const getServerSideProps = withAuth(async ({ req, res }, user) => {
+  const title = await getTitle()
+  return title
+})
 
 export default SecretSSR
