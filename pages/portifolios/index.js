c0017a139569c409960dbc56f925dabb80cc35ea
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-05 13:19:33 -0300

    feat: Portifolio Card

--- pages/portifolios/index.js
+++ pages/portifolios/index.js
@@ -2,52 +2,21 @@ import { useGetUser } from '@/actions/user'
 import BasePage from '@/components/BasePage'
 import BaseLayout from '@/components/layouts/BaseLayout'
 import PortfolioApi from '@/lib/api/portfolios'
-import { Fragment } from 'react'
-import {
-  Card,
-  CardBody,
-  CardHeader,
-  CardText,
-  CardTitle,
-  Col,
-  Row
-} from 'reactstrap'
+import { Col, Row } from 'reactstrap'
+import PortfolioCard from '../../components/PortifolioCard'
 
 const Portfolios = ({ portfolios }) => {
   const { data: dataU, loading: loadingU } = useGetUser()
-
-  const renderPortfolios = (portfolios) =>
-    portfolios.map((portfolio) => (
-      <Col md="4" key={portfolio._id}>
-        <Fragment key={portfolio._id}>
-          <span>
-            <Card className="portfolio-card">
-              <CardHeader className="portfolio-card-header">
-                Some Position
-              </CardHeader>
-              <CardBody>
-                <p className="portfolio-card-city">Some Location</p>
-                <CardTitle className="portfolio-card-title">
-                  Some Company
-                </CardTitle>
-                <CardText className="portfolio-card-text">
-                  Some Description
-                </CardText>
-                <div className="readMore"> </div>
-              </CardBody>
-            </Card>
-          </span>
-        </Fragment>
-      </Col>
-    ))
 
   return (
     <BaseLayout user={dataU} loading={loadingU}>
       <BasePage className="portfolio-page" title="Portifolio">
         <Row>
-          {/* {loading === true && <p>Loading data...</p>}
-          {error && <div className="alert alert-danger">{error.message}</div>} */}
-          {portfolios && renderPortfolios(portfolios)}
+          {portfolios.map((portfolio) => (
+            <Col key={portfolio._id} md="4">
+              <PortfolioCard portfolio={portfolio} />
+            </Col>
+          ))}
         </Row>
       </BasePage>
     </BaseLayout>
