52f13c5d0fcb6710c63a8f360efba690dcb2ee7f
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-04 23:12:29 -0300

    feat: server side validating if user is authorized

--- utils/auth0.js
+++ utils/auth0.js
@@ -14,6 +14,10 @@ const auth0 = initAuth0({
 
 export default auth0
 
+export const isAuthorized = (user, role) => {
+  return user && user[process.env.AUTH0_NAMESPACE + '/roles'].includes(role)
+}
+
 export const authorizedUser = async (req, res) => {
   const session = await auth0.getSession(req)
   if (!session || !session.user) {
@@ -25,9 +29,13 @@ export const authorizedUser = async (req, res) => {
   return session.user
 }
 
-export const withAuth = (getData) => async ({ req, res }) => {
+export const withAuth = (getData) => (role) => async ({ req, res }) => {
   const session = await auth0.getSession(req)
-  if (!session || !session.user) {
+  if (
+    !session ||
+    !session.user ||
+    (role && !isAuthorized(session.user, role))
+  ) {
     res.writeHead(302, { Location: '/api/v1/login' })
     res.end()
     return { props: {} }
