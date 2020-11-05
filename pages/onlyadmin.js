60b1af1f37d2d68d7911a082fe54befe14825c03
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-04 23:07:54 -0300

    feat: Only admin page

---
+++ pages/onlyadmin.js
@@ -0,0 +1,16 @@
+import BasePage from '@/components/BasePage'
+import BaseLayout from '@/components/layouts/BaseLayout'
+import withAuth from '@/hoc/withAuth'
+
+const OnlyAdmin = ({ user, loading }) => {
+  return (
+    <BaseLayout user={user} loading={loading}>
+      <BasePage className="secret-page" title="I am About Page">
+        <h1>I am Admin Page</h1>
+        <p>Secret Content Here - Hello {user.name}</p>
+      </BasePage>
+    </BaseLayout>
+  )
+}
+
+export default withAuth(OnlyAdmin)('admin')
