0be28873086eb57d707e3fe29cb6411024e67b89
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-04 23:07:30 -0300

    feat: Change from class to functional component and change path of images

--- pages/index.js
+++ pages/index.js
@@ -1,84 +1,74 @@
-import { Component } from "react";
-import Typed from "react-typed";
-import { Col, Container, Row } from "reactstrap";
-import BaseLayout from "../components/layouts/BaseLayout";
+import { useGetUser } from '@/actions/user'
+import Typed from 'react-typed'
+import { Col, Container, Row } from 'reactstrap'
+import BaseLayout from '../components/layouts/BaseLayout'
 
-class Index extends Component {
-  constructor(props) {
-    super(props);
+const ROLES = [
+  'Developer',
+  'Tech Lover',
+  'Team Player',
+  'Course Creater',
+  'React.js',
+  'Node.js'
+]
+const Index = () => {
+  const { data, loading } = useGetUser()
 
-    this.roles = [
-      "Developer",
-      "Tech Lover",
-      "Team Player",
-      "Course Creater",
-      "React.js",
-      "Angular"
-    ];
-  }
+  return (
+    <BaseLayout user={data} loading={loading} className="cover">
+      <div className="main-section">
+        <div className="background-image">
+          <img src="/images/background-index.png" />
+        </div>
 
-  render() {
-    const { isAuthenticated, user } = this.props.auth;
-    return (
-      <BaseLayout className="cover" {...this.props.auth} headerType="index">
-        <div className="main-section">
-          <div className="background-image">
-            <img src="/static/images/background-index.png" />
-          </div>
-
-          <Container>
-            <Row>
-              <Col md="6">
-                <div className="hero-section">
-                  <div className={`flipper`}>
-                    <div className="back">
-                      <div className="hero-section-content">
-                        <h2> Full Stack Web Developer </h2>
-                        <div className="hero-section-content-intro">
-                          Have a look at my portfolio and job history.
-                        </div>
+        <Container>
+          <Row>
+            <Col md="6">
+              <div className="hero-section">
+                <div className={`flipper`}>
+                  <div className="back">
+                    <div className="hero-section-content">
+                      <h2> Full Stack Web Developer </h2>
+                      <div className="hero-section-content-intro">
+                        Have a look at my portfolio and job history.
                       </div>
-                      <img
-                        className="image"
-                        src="/static/images/section-1.png"
-                      />
-                      <div className="shadow-custom">
-                        <div className="shadow-inner"> </div>
-                      </div>
+                    </div>
+                    <img className="image" src="/images/section-1.png" />
+                    <div className="shadow-custom">
+                      <div className="shadow-inner"> </div>
                     </div>
                   </div>
                 </div>
-              </Col>
-              <Col md="6" className="hero-welcome-wrapper">
-                <div className="hero-welcome-text">
-                  <h1>
-                    {isAuthenticated && <b>{user.name} </b>}
-                    Welcome to the portfolio website of Filip Jerga. Get
-                    informed, collaborate and discover projects I was working on
-                    through the years!
-                  </h1>
-                </div>
-                <Typed
-                  loop
-                  typeSpeed={60}
-                  backSpeed={60}
-                  strings={this.roles}
-                  backDelay={1000}
-                  loopCount={0}
-                  showCursor
-                  className="self-type"
-                  cursorChar="|"
-                />
-                <div className="hero-welcome-bio">
-                  <h1>Let's take a look on my work.</h1>
-                </div>
-              </Col>
-            </Row>
-          </Container>
-        </div>
-      </BaseLayout>
-    );
-  }
+              </div>
+            </Col>
+            <Col md="6" className="hero-welcome-wrapper">
+              <div className="hero-welcome-text">
+                <h1>
+                  Welcome to the portfolio website of Raul Rosa. Get informed,
+                  collaborate and discover projects I was working on through the
+                  years!
+                </h1>
+              </div>
+              <Typed
+                loop
+                typeSpeed={70}
+                backSpeed={70}
+                strings={ROLES}
+                backDelay={1000}
+                loopCount={0}
+                showCursor
+                className="self-type"
+                cursorChar="|"
+              />
+              <div className="hero-welcome-bio">
+                <h1>Let's take a look on my work.</h1>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </BaseLayout>
+  )
 }
 
-export default Index;
+export default Index
