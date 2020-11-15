4083fd45f360f8d65844ec4f08ee398aabb3d827
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-14 22:58:21 -0300

    feat: action to update portfolio

--- actions/portfolios.js
+++ actions/portfolios.js
@@ -3,8 +3,11 @@ import axios from 'axios'
 import useSWR from 'swr'
 
 const createPortfolio = (data) => axios.post('/api/v1/portfolios', data)
+const updatePortfolio = (id, data) =>
+  axios.patch(`/api/v1/portfolios/${id}`, data)
 
 export const useCreatePortfolio = () => useApiHandler(createPortfolio)
+export const useUpdatePortfolio = () => useApiHandler(updatePortfolio)
 
 export const useGetPortfolio = (id) => {
   const { data, error, ...rest } = useSWR(
