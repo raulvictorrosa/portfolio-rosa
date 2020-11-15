f79d8e77c23a300c7707b40b4452eacbdbab9d1c
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-15 16:12:38 -0300

    chore: no more necessary in version 9.4.0 of next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const path = require('path')
-const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname)
-    config.plugins.push(new Dotenv({ silent: true }))
 
     return config
   },
