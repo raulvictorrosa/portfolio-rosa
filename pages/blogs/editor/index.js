2d6fd9a6ee4c301b24756c1b328e95bdbfc56afb
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-15 15:59:49 -0300

    feat: blog editor page

---
+++ pages/blogs/editor/index.js
@@ -0,0 +1,13 @@
+import BasePage from '@/components/BasePage'
+import BaseLayout from '@/components/layouts/BaseLayout'
+import withAuth from '@/hoc/withAuth'
+
+const BlogEditor = ({ user, loading }) => {
+  return (
+    <BaseLayout user={user} loading={loading}>
+      <BasePage className="blog-editor" title="Blog Editor" />
+    </BaseLayout>
+  )
+}
+
+export default withAuth(BlogEditor)('admin')
