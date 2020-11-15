26d54e652fd479a4329e8f6a8494da45cf5a0b38
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-14 23:48:52 -0300

    feat: include try catch on handlePortfolio

--- pages/api/v1/portfolios/[id].js
+++ pages/api/v1/portfolios/[id].js
@@ -3,16 +3,24 @@ import auth0 from '@/utils/auth0'
 
 export default async function handlePortfolio(req, res) {
   if (req.method === 'GET') {
-    const json = await new PortfolioApi().getById(req.query.id)
-    return res.json(json.data)
+    try {
+      const json = await new PortfolioApi().getById(req.query.id)
+      return res.json(json.data)
+    } catch (error) {
+      return res.status(error.status || 422).json(error.response.data)
+    }
   }
 
   if (req.method === 'PATCH') {
-    const { accessToken } = await auth0.getSession(req)
-    const json = await new PortfolioApi(accessToken).update(
-      req.query.id,
-      req.body
-    )
-    return res.json(json.data)
+    try {
+      const { accessToken } = await auth0.getSession(req)
+      const json = await new PortfolioApi(accessToken).update(
+        req.query.id,
+        req.body
+      )
+      return res.json(json.data)
+    } catch (error) {
+      return res.status(error.status || 422).json(error.response.data)
+    }
   }
 }
