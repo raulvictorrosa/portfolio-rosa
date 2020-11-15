994a76bf69f8238a3cf9621f744f387512e0f543
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-14 23:00:37 -0300

    feat: handle get portfolio by id and update portfolio

--- pages/api/v1/portfolios/[id].js
+++ pages/api/v1/portfolios/[id].js
@@ -1,12 +1,18 @@
 import PortfolioApi from '@/lib/api/portfolios'
-// import auth0 from '@/utils/auth0'
+import auth0 from '@/utils/auth0'
 
 export default async function handlePortfolio(req, res) {
-  // try {
-  // const { accessToken } = await auth0.getSession(req)
-  const json = await new PortfolioApi().getById(req.query.id)
-  return res.json(json.data)
-  // } catch (error) {
-  // return res.status(error.status || 422).json(error.response.data)
-  // }
+  if (req.method === 'GET') {
+    const json = await new PortfolioApi().getById(req.query.id)
+    return res.json(json.data)
+  }
+
+  if (req.method === 'PATCH') {
+    const { accessToken } = await auth0.getSession(req)
+    const json = await new PortfolioApi(accessToken).update(
+      req.query.id,
+      req.body
+    )
+    return res.json(json.data)
+  }
 }
