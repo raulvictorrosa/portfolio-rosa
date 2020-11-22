3fed8d2ec740e3a22e0954ede4eb6d9eb87b5bed
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-22 11:30:52 -0300

    chore: next config for locales

--- next.config.js
+++ next.config.js
@@ -3,5 +3,9 @@ const path = require('path')
 module.exports = {
   env: {
     AUTH0_NAMESPACE: process.env.AUTH0_NAMESPACE
+  },
+  i18n: {
+    locales: ['en', 'pt-br'],
+    defaultLocale: 'en'
   }
 }
