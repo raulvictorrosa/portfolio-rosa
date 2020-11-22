0459159014fbec34b413368a90e9ed2254e556f8
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-22 20:03:39 -0300

    feat: load slate-simple-editor style

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-toastify/dist/ReactToastify.css'
+import 'slate-simple-editor/dist/index.css'
 import 'styles/main.scss'
 
 const App = ({ Component, pageProps }) => <Component {...pageProps} />
