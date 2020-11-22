5252957d4f7e82f416228b40f88677b0406791e3
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-22 20:02:04 -0300

    feat: shows editor with function to create a blog post

--- pages/blogs/editor/index.js
+++ pages/blogs/editor/index.js
@@ -1,11 +1,35 @@
+import { useCreateBlog } from 'actions/blogs'
 import BasePage from 'components/BasePage'
 import BaseLayout from 'components/layouts/BaseLayout'
 import withAuth from 'hoc/withAuth'
+import { useRouter } from 'next/router'
+import { toast } from 'react-toastify'
+import { Editor } from 'slate-simple-editor'
 
 const BlogEditor = ({ user, loading }) => {
+  const router = useRouter()
+  const [
+    createBlog,
+    { data: createdBlog, error, loading: blogLoading }
+  ] = useCreateBlog()
+
+  const saveBlog = async (data) => {
+    const createdBlog = await createBlog(data)
+    router.push(`/blogs/editor/${createdBlog._id}`)
+    alert('Blog was created!')
+  }
+
+  if (error) {
+    toast.error(error.message)
+  }
+
   return (
     <BaseLayout user={user} loading={loading}>
-      <BasePage className="blog-editor" title="Blog Editor" />
+      <BasePage className="blog-editor" title="Blog Editor">
+        {typeof window !== 'undefined' && (
+          <Editor onSave={saveBlog} loading={blogLoading} />
+        )}
+      </BasePage>
     </BaseLayout>
   )
 }
