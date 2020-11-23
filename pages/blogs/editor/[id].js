9e5938abfc1ff047a68b8a6ec990ac5e918d84b1
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-22 22:30:06 -0300

    feat: fuunction to update blog post

--- pages/blogs/editor/[id].js
+++ pages/blogs/editor/[id].js
@@ -1,13 +1,27 @@
-import { useGetBlog } from 'actions/blogs'
+import { useGetBlog, useUpdateBlog } from 'actions/blogs'
 import BasePage from 'components/BasePage'
 import BaseLayout from 'components/layouts/BaseLayout'
 import withAuth from 'hoc/withAuth'
 import { useRouter } from 'next/router'
+import { toast } from 'react-toastify'
 import { Editor } from 'slate-simple-editor'
 
 const BlogUpdateEditor = ({ user, loading }) => {
   const router = useRouter()
-  const { data, error } = useGetBlog(router.query.id)
+  const { data } = useGetBlog(router.query.id)
+  const [
+    updateBlog,
+    { data: updatedBlog, error, loading: isBlogSaving }
+  ] = useUpdateBlog(router.query.id)
+
+  const _updateBlog = async (data) => {
+    await updateBlog(router.query.id, data)
+    toast.success('Blog updated!')
+  }
+
+  if (error) {
+    toast.error(error)
+  }
 
   return (
     <BaseLayout user={user} loading={loading}>
@@ -16,7 +30,8 @@ const BlogUpdateEditor = ({ user, loading }) => {
           <Editor
             header="Update Your Blog..."
             initialContent={data.content}
-            onSave={() => {}}
+            onSave={_updateBlog}
+            loading={isBlogSaving}
           />
         )}
       </BasePage>
