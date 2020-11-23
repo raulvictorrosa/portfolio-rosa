6768c17c062271a8b50489a7f1a07da50dd5eb41
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-22 23:28:31 -0300

    feat: get post by user

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,26 @@
 import BasePage from 'components/BasePage'
 import BaseLayout from 'components/layouts/BaseLayout'
 import Masthead from 'components/shared/Masthead'
+import BlogApi from 'lib/api/blogs'
+import Link from 'next/link'
 import { Col, Row } from 'reactstrap'
-import { withAuth } from 'utils/auth0'
+import auth0, { withAuth } from 'utils/auth0'
 
-const Dashboard = ({ user, loading }) => {
+const Dashboard = ({ user, blogs, loading }) => {
+  const renderBlogs = (blogs, status) => (
+    <ul className="user-blogs-list">
+      {blogs
+        .filter((blog) => blog.status === status)
+        .map((blog) => (
+          <li key={blog._id}>
+            <Link href={`/blogs/editor/${blog._id}`}>
+              <a>{blog.title}</a>
+            </Link>
+          </li>
+        ))}
+    </ul>
+  )
+
   return (
     <BaseLayout navClass="transparent" user={user} loading={false}>
       <Masthead imagePath="/images/home-bg.jpg" />
@@ -12,9 +28,11 @@ const Dashboard = ({ user, loading }) => {
         <Row>
           <Col md="6" className="mx-auto text-center">
             <h2 className="blog-status-title"> Published Blogs </h2>
+            {renderBlogs(blogs, 'published')}
           </Col>
           <Col md="6" className="mx-auto text-center">
             <h2 className="blog-status-title"> Draft Blogs </h2>
+            {renderBlogs(blogs, 'draft')}
           </Col>
         </Row>
       </BasePage>
@@ -22,6 +40,10 @@ const Dashboard = ({ user, loading }) => {
   )
 }
 
-export const getServerSideProps = withAuth()('admin')
+export const getServerSideProps = withAuth(async ({ req, res }, user) => {
+  const { accessToken } = await auth0.getSession(req)
+  const json = await new BlogApi(accessToken).getByUser()
+  return { blogs: json.data }
+})('admin')
 
 export default Dashboard
