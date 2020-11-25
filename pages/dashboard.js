9a1dd5cc4a59f4e1d58d38d7e56eaf877c5fb382
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-25 14:36:48 -0300

    feat: some enhancement when updating post status

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,18 +1,21 @@
-import { useUpdateBlog } from 'actions/blogs'
+import { useGetUserBlogs, useUpdateBlog } from 'actions/blogs'
 import BasePage from 'components/BasePage'
 import BaseLayout from 'components/layouts/BaseLayout'
 import PortfolioDropdown from 'components/shared/Dropdown'
 import Masthead from 'components/shared/Masthead'
-import BlogApi from 'lib/api/blogs'
+import withAuth from 'hoc/withAuth'
 import Link from 'next/link'
+import { toast } from 'react-toastify'
 import { Col, Row } from 'reactstrap'
-import auth0, { withAuth } from 'utils/auth0'
 
-const Dashboard = ({ user, blogs, loading }) => {
+const Dashboard = ({ user, loading }) => {
   const [updateBlog] = useUpdateBlog()
+  const { data: blogs, mutate } = useGetUserBlogs()
 
   const changeBlogStatus = async (blogId, status) => {
     await updateBlog(blogId, { status })
+      .then(() => mutate())
+      .catch((err) => toast.error('Something went wrong!'))
   }
 
   const createOption = (blogStatus) => {
@@ -40,21 +43,22 @@ const Dashboard = ({ user, blogs, loading }) => {
 
   const renderBlogs = (blogs, status) => (
     <ul className="user-blogs-list">
-      {blogs
-        .filter((blog) => blog.status === status)
-        .map((blog) => (
-          <li key={blog._id}>
-            <Link href={`/blogs/editor/${blog._id}`}>
-              <a>{blog.title}</a>
-            </Link>
-            <PortfolioDropdown items={createOptions(blog)} />
-          </li>
-        ))}
+      {blogs &&
+        blogs
+          .filter((blog) => blog.status === status)
+          .map((blog) => (
+            <li key={blog._id}>
+              <Link href={`/blogs/editor/${blog._id}`}>
+                <a>{blog.title}</a>
+              </Link>
+              <PortfolioDropdown items={createOptions(blog)} />
+            </li>
+          ))}
     </ul>
   )
 
   return (
-    <BaseLayout navClass="transparent" user={user} loading={false}>
+    <BaseLayout navClass="transparent" user={user} loading={loading}>
       <Masthead imagePath="/images/home-bg.jpg" />
       <BasePage className="dashboard-page blog-user-page">
         <Row>
@@ -72,10 +76,4 @@ const Dashboard = ({ user, blogs, loading }) => {
   )
 }
 
-export const getServerSideProps = withAuth(async ({ req, res }, user) => {
-  const { accessToken } = await auth0.getSession(req)
-  const json = await new BlogApi(accessToken).getByUser()
-  return { blogs: json.data }
-})('admin')
-
-export default Dashboard
+export default withAuth(Dashboard)('admin')
