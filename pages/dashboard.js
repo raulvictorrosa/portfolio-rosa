f40402ac243fd4221d5fd2e28502de9b0875d5fe
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-25 14:10:22 -0300

    feat: functions to change post status

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,5 +1,7 @@
+import { useUpdateBlog } from 'actions/blogs'
 import BasePage from 'components/BasePage'
 import BaseLayout from 'components/layouts/BaseLayout'
+import PortfolioDropdown from 'components/shared/Dropdown'
 import Masthead from 'components/shared/Masthead'
 import BlogApi from 'lib/api/blogs'
 import Link from 'next/link'
@@ -7,6 +9,35 @@ import { Col, Row } from 'reactstrap'
 import auth0, { withAuth } from 'utils/auth0'
 
 const Dashboard = ({ user, blogs, loading }) => {
+  const [updateBlog] = useUpdateBlog()
+
+  const changeBlogStatus = async (blogId, status) => {
+    await updateBlog(blogId, { status })
+  }
+
+  const createOption = (blogStatus) => {
+    return blogStatus === 'draft'
+      ? { view: 'Publish Story', value: 'published' }
+      : { view: 'Make a Draft', value: 'draft' }
+  }
+
+  const createOptions = (blog) => {
+    const option = createOption(blog.status)
+
+    return [
+      {
+        key: `${blog._id}-publish`,
+        text: option.view,
+        handlers: { onClick: () => changeBlogStatus(blog._id, option.value) }
+      },
+      {
+        key: `${blog._id}-delete`,
+        text: 'Delete',
+        handlers: { onClick: () => alert('Clicking delete') }
+      }
+    ]
+  }
+
   const renderBlogs = (blogs, status) => (
     <ul className="user-blogs-list">
       {blogs
@@ -16,6 +47,7 @@ const Dashboard = ({ user, blogs, loading }) => {
             <Link href={`/blogs/editor/${blog._id}`}>
               <a>{blog.title}</a>
             </Link>
+            <PortfolioDropdown items={createOptions(blog)} />
           </li>
         ))}
     </ul>
