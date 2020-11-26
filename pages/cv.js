42ecc3f02ec624d6b108ffefb182ef5fe61b4e63
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-26 20:29:56 -0300

    feat: show pdf cv on cv page

--- pages/cv.js
+++ pages/cv.js
@@ -1,10 +1,18 @@
 import BasePage from 'components/BasePage'
 import BaseLayout from 'components/layouts/BaseLayout'
+import { Col, Row } from 'reactstrap'
 
 const Cv = () => (
   <BaseLayout>
     <BasePage>
-      <h1>I am Cv Page</h1>
+      <Row>
+        <Col md={{ size: 8, offset: 2 }}>
+          <iframe
+            style={{ width: '100%', height: '800px' }}
+            src="/Raul Rosa.pdf"
+          />
+        </Col>
+      </Row>
     </BasePage>
   </BaseLayout>
 )
