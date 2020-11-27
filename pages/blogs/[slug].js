24064572a46d485ef5c3f0b00c4e0a117677f569
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-26 22:23:59 -0300

    feat: get post author information

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -5,7 +5,7 @@ import BlogApi from 'lib/api/blogs'
 import { Col, Row } from 'reactstrap'
 import { SlateView } from 'slate-simple-editor'
 
-const BlogDetail = ({ blog }) => {
+const BlogDetail = ({ blog, author }) => {
   const { data, loading } = useGetUser()
   return (
     <BaseLayout user={data} loading={loading}>
@@ -32,8 +32,10 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const json = await new BlogApi().getBySlug(params.slug)
-  return { props: { blog: json.data } }
+  const {
+    data: { blog, user: author }
+  } = await new BlogApi().getBySlug(params.slug)
+  return { props: { blog, author } }
 }
 
 export default BlogDetail
